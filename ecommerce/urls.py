c8b60389e51997c44841c0ba9baf3317947d4511
Ayse Kubra <aysekubrakupeli@gmail.com>
2017-11-22 15:19:31 +0000

    Checkout function added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,12 +21,14 @@ from django.views import static
 from .settings import MEDIA_ROOT
 from cart import urls as cart_urls
 from products import urls as products_urls
+from checkout import urls as checkout_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', all_products, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
+    url(r'^checkout/', include(checkout_urls)),
     url(r'^cart/', include(cart_urls)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
